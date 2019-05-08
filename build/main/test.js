0010cb82243f51febacd318cbc775e4a56aa6939
Tobias Hegemann <t.hegemann@mirador-kommunikation.de>
2019-05-08 17:29:57 +0200

    current

--- build/main/test.js
+++ build/main/test.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Contao_1 = __importDefault(require("./lib/Contao"));
-console.log("HELLO WORLD");
-const contao = new Contao_1.default("https://valensina-kids.tobiashegemann.de");
+const url = "test.com";
+const contao = new Contao_1.default(url);
 contao.getStartPage();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy90ZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsMERBQWtDO0FBRWxDLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxDQUFDLENBQUM7QUFFM0IsTUFBTSxNQUFNLEdBQVcsSUFBSSxnQkFBTSxDQUFDLDBDQUEwQyxDQUFDLENBQUM7QUFDOUUsTUFBTSxDQUFDLFlBQVksRUFBRSxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy90ZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsMERBQWtDO0FBRWxDLE1BQU0sR0FBRyxHQUFHLFVBQVUsQ0FBQztBQUV2QixNQUFNLE1BQU0sR0FBVyxJQUFJLGdCQUFNLENBQUMsR0FBRyxDQUFDLENBQUM7QUFDdkMsTUFBTSxDQUFDLFlBQVksRUFBRSxDQUFDIn0=
